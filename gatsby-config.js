f12030a2f1c3a86bea589c0ceb174663a067fec4
Brandon Noad <Brandon.Noad@gmail.com>
2021-04-03 17:28:40 -0300

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,19 @@ module.exports = {
                 isResettingCSS: true,
                 isUsingColorMode: false
             }
+        },
+        {
+            // Generates favicon
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Standards for Rigor and Transparency in Dysphagia Research',
+                short_name: 'STARTED',
+                start_url: '/',
+                background_color: '#fff',
+                theme_color: '#3F9142',
+                display: 'standalone',
+                icon: 'src/images/favicon.png'
+            }
         }
     ]
 };
