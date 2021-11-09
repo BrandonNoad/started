3597edb042dd6e7584627d749216b4c4e504c945
Brandon Noad <Brandon.Noad@gmail.com>
2021-11-09 10:50:33 -0400

    Add survey alert with link

--- src/layouts/BlogLayout.js
+++ src/layouts/BlogLayout.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { Box, Flex, Button, Badge } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import {
+    Box,
+    Flex,
+    Button,
+    Badge,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+    CloseButton,
+    Link
+} from '@chakra-ui/react';
 
 import Seo from '../components/Seo';
 import Logo from '../components/Logo';
@@ -9,10 +20,38 @@ import Footer from '../components/Footer';
 const commonPx = [3, 8, 12];
 
 const BlogLayout = ({ title, children }) => {
+    const [isAlertVisible, setIsAlertVisible] = useState(true);
+
     return (
         <>
             <Seo title={title}></Seo>
             <Flex direction="column" minHeight="full">
+                {title !== 'About' && isAlertVisible ? (
+                    <Alert colorScheme="secondary" justifyContent="center">
+                        <AlertIcon />
+                        <AlertTitle mr={2}>We want your feedback!</AlertTitle>
+                        <AlertDescription>
+                            To provide feedback on the STARTED Framework, please complete{' '}
+                            <Link
+                                color="secondary.700"
+                                href="https://uwmadison.co1.qualtrics.com/jfe/form/SV_9SH5Q0b7ZpfTH6u"
+                                isExternal={true}
+                            >
+                                our survey
+                            </Link>
+                            .
+                        </AlertDescription>
+
+                        <CloseButton
+                            position="absolute"
+                            right="8px"
+                            top="8px"
+                            onClick={() => {
+                                setIsAlertVisible(false);
+                            }}
+                        />
+                    </Alert>
+                ) : null}
                 <Flex
                     as="header"
                     align="center"
@@ -24,7 +63,13 @@ const BlogLayout = ({ title, children }) => {
                 >
                     <Logo />
                     <Box>
-                        <Button colorScheme="gray" variant="ghost" as={Link} to="/about" mr={2}>
+                        <Button
+                            colorScheme="gray"
+                            variant="ghost"
+                            as={GatsbyLink}
+                            to="/about"
+                            mr={2}
+                        >
                             About
                         </Button>
                         <Badge colorScheme="orange" variant="solid" fontSize="md">
